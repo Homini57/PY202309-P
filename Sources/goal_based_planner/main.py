a0d96823827c4d3c0c17cf23c03726d258b3f09f
unknown <200244@jnu.ac.kr>
2023-12-10 21:52:46 +0900

    Áøôº¸°� À2 Á¦Ãâ Àü ÃÖÁ¾ ¼öÁ¤ ¹× Áøôº¸°� ¾÷·εå

--- Sources/goal_based_planner/main.py
+++ Sources/goal_based_planner/main.py
@@ -3,22 +3,19 @@ from page_py.class_page import *
 
 
 if __name__ == '__main__':
-    #start_page = open_todays_goal(goal_data_field)
+    # 프로그램 시작 : 첫번째 페이지를 열린 페이지에 추가
     page = Page()
     start_page = MainGoalPage()
     page.opened_pages.append(start_page)
+    # 열려있는 페이지의 내용을 업데이트 후, 마지막 페이지를 실행, 반복
     while True:
         last_index = len(page.opened_pages) - 1
         next_page = page.opened_pages[last_index]
         next_page.update_page()
-        terminate = next_page.open_page() # 다음 페이지 생성 or 현재 페이지 삭제 or 종료 메시지?
+        terminate = next_page.open_page()
+        # 종료 메시지 입력시 현재까지 수정된 데이터 저장 후 프로그램 종료
         if terminate == True:
             write_data_file(GoalField().data)
             write_contents_file(GoalField().contents)
+            print('프로그램을 종료합니다.')
             break
-
-#오픈 함수 : 실행 후 페이지 생성
-# 이전 : 현재,이전 페이지 삭제, 이전 페이지 실행
-# 실행 : 실행
-# 종료 : 종료
-#1. 실행함수 2.종료 3. 이전
--- Sources/goal_based_planner/page_py/class_page.py
+++ Sources/goal_based_planner/page_py/class_page.py
@@ -7,18 +7,17 @@ from goal_py.class_goal import Goal, GoalField, to_digit_number
 class Page():
     opened_pages = []
     # 필드 : 페이지 제목, 목표 레벨, 출력 메뉴 항목
-    # 출력 타입( 0 : 일반 페이지, 1 : 오늘 목표 페이지, 2 : 메인 목표 페이지)
     def __init__(self, title = '', menu_list = []):
         self.title = title
         self.menu_list = menu_list
         self.basic_menu = [' +. 추가           ','-. 삭제           ','enter. 세부 내용', \
                            '-1. 이전으로       ','0. 메인으로       ','    ?. 도움말  ', \
                             'esc.프로그램 종료']
+    # 페이지 내용 업데이트
     def update_page(self):
         self.__init__(self.title, self.menu_list)
-    # 제목과 기본 메뉴를 출력 페이지 앞부분에 출력
-    # C++의 가상함수처럼 이것도 상속으로 다르게 정의하고 같은 페이지 리스트에 저장할 수 있을까?
-    def print_menu(self):
+    # 페이지 출력
+    def print_page(self):
         
         print('')
         print('===================================================')
@@ -34,24 +33,22 @@ class Page():
         for i in range(0,len(self.menu_list)):
             print(f"{i+1}. {self.menu_list[i]}")
         print('\n')
-        
+    # 페이지 열기(상속)
     def open_page(self):
         pass
-    
-        
-
+    # 열린 페이지에 추가
     def in_opened_pages(self,page):
         self.opened_pages.append(page)
-
-
+    # 마지막 페이지 삭제
     def delete_page(self):
         last_index = len(self.opened_pages) - 1
         if last_index > 0:
             del self.opened_pages[last_index]
         else:
             print('이전 페이지가 없습니다.')
+# 매우 중요! : 0 입력시 실행될 페이지의 타입을 입력받고, 오픈페이지 메서드를 통합하는 방법 고려
 
-
+# 목표 기반 페이지
 class GoalPage(Page):
     def __init__(self, selected_goal):
         # 선택한 목표 데이터 중 목표 내용, 하위 목표 항목 생성
@@ -61,14 +58,13 @@ class GoalPage(Page):
                          if goal.get_higher_goal_number() == selected_goal.goal_number]
         sub_goal_contents = [goal.get_content() for goal in self.sub_goal_list]
         super().__init__(goal_content, sub_goal_contents)
-
+    # 페이지 업데이트
     def update_page(self):
         self.__init__(self.goal)
-
+    # 페이지 열기
     def open_page(self):
-        self.print_menu()
+        self.print_page()
         command = input()
-        #return_message = RunMenu(goal_data_field, todays_goal_list, command)
         if(command == ''):
             pass
         elif(command == '+'):
@@ -77,15 +73,17 @@ class GoalPage(Page):
         elif(command == '-'):
             next_page = DeleteGoalPage(self.goal)
             self.in_opened_pages(next_page)
+        # 0 입력시 오늘 목표 페이지 생성
         elif(command == '0'):
             next_page = MainGoalPage()
             self.in_opened_pages(next_page)
         elif(command == '-1'):
             self.delete_page()
+        # esc 입력시 프로그램 종료 값 반환
         elif(command == 'esc'):
-            return True
+            terminate = True
+            return terminate
         # 1 이상의 정수를 입력받을 경우 해당 목표 페이지 열기
-        # 같은 코드 반복, 함수화 고려
         else:
             try:
                 int_command = int(command)
@@ -98,7 +96,8 @@ class GoalPage(Page):
                     self.in_opened_pages(next_page)
                     return False
             print('잘못 입력하였습니다.')
-        return False
+        terminate = False
+        return terminate
 
 class MainGoalPage(GoalPage):
     def __init__(self):
@@ -113,9 +112,8 @@ class MainGoalPage(GoalPage):
         self.__init__()
 
     def open_page(self):
-        self.print_menu()
+        self.print_page()
         command = input()
-        #return_message = RunMenu(goal_data_field, todays_goal_list, command)
         if(command == ''):
             print('세부 내용이 없습니다.')
         elif(command == '+'):
@@ -124,14 +122,15 @@ class MainGoalPage(GoalPage):
         elif(command == '-'):
             next_page = DeleteGoalPage(self.goal)
             self.in_opened_pages(next_page)
+        # 0 입력시 오늘 목표 페이지 생성
         elif(command == '0'):
-            pass #next_page = MainDatePage()
+            pass #next_page = TodaysGoalPage()
         elif(command == '-1'):
             self.delete_page()
         elif(command == 'esc'):
-            return True
+            terminate = True
+            return terminate
         # 1 이상의 정수를 입력받을 경우 해당 목표 페이지 열기
-        # 같은 코드 반복, 함수화 고려
         else:
             try:
                 int_command = int(command)
@@ -144,49 +143,82 @@ class MainGoalPage(GoalPage):
                     self.in_opened_pages(next_page)
                     return False
             print('잘못 입력하였습니다.')
-        return False
+        terminate = False
+        return terminate
 
+# 목표 추가, 삭제 페이지
 class EditGoalPage(GoalPage):
-    # 제목과 기본 메뉴를 출력 페이지 앞부분에 출력
+
     def __init__(self, goal):
         super().__init__(goal)
         # 이 조건은 왜 self.goal == Goal()이 안되는 건지?
+        # 메인 목표 페이지에서 실행시 첫 하위 목표(메인 목표를 나타내는 목표) 삭제
         if self.goal.goal_number == Goal().goal_number:
             del self.sub_goal_list[0]
             del self.menu_list[0]
+        # 기본 메뉴 : -1, 0, ?, esc
         for i in range(0,3):
             del self.basic_menu[0]
-
+    # 페이지 업데이트
     def update_page(self):
         self.__init__(self.goal)
-
-    def print_menu(self, edit_title):
+    # 편집 제목 출력 + 페이지 출력
+    def print_page(self, edit_title):
         self.title = edit_title + '\n\n' + self.title
-        super().print_menu()
-        # self.title이 내용인 목표의 번호를 필드에서 찾기 (GetGoal(contents).goal_number 활용)
-        # 목표 번호의 하위 항목 중 가장 마지막 번호 찾기 + (위의 goal_number == Goal().get_higher_goal_number()인 Goal()클래스 찾기)
-        # 해당 번호의 각 필드에 새로운 목표 생성, 입력을 내용에 저장(GoalField().data.append(Goal(goal_number))), GoalField().contents[goal_number] = contents)
+        super().print_page()
 
+
+
+# 목표 추가 페이지
 class AddGoalPage(EditGoalPage):
+    # 페이지 열기
     def open_page(self):
-        self.print_menu('목표 추가')
+        # 페이지 출력
+        self.print_page('목표 추가')
         command = input()
-        #return_message = RunMenu(goal_data_field, todays_goal_list, command)
+        # 0 입력시 메인 목표 페이지로 이동
         if(command == '0'):
             next_page = MainGoalPage()
             self.in_opened_pages(next_page)
         elif(command == '-1'):
             self.delete_page()
         elif(command == 'esc'):
-            return True
-        # 1 이상의 정수를 입력받을 경우 해당 목표 페이지 열기
-        # 같은 코드 반복, 함수화 고려
+            terminate = True
+            return terminate
+        # 입력받은 내용을 목표에 추가
+        else:
+            # 마지막 하위 목표 탐색
+            current_goal_number = self.goal.goal_number
+            last_sub_goal_index = len(self.sub_goal_list) - 1
+            # 목표 데이터 필드 상 마지막 하위 목표의 인덱스 계산, 새 목표의 마지막 번호 계산
+            # 하위 목표가 존재할 경우 생성 방법
+            if last_sub_goal_index >= 0:
+                last_sub_goal = self.sub_goal_list[last_sub_goal_index]
+                goal_level = last_sub_goal.get_goal_level()
+                last_part_number = last_sub_goal.get_part_number(goal_level)
+                new_part_number = to_digit_number(int(last_part_number)+1, last_sub_goal.DIGIT_SIZE)
+                last_index = GoalField().data.index(last_sub_goal)
+            # 하위 목표가 없는 경우 현재 페이지의 목표의 인덱스로 계산
+            else:
+                new_part_number = '01'
+                last_index = GoalField().data.index(self.goal)
+            # 새 목표의 목표 번호 계산
+            new_goal_number = current_goal_number + new_part_number
+            # 위치에 맞게 데이터 필드에 새 목표 입력, 내용 필드에 내용 추가
+            GoalField().data.insert(last_index+1,Goal(new_goal_number))
+            GoalField().contents[new_goal_number] = command
+        terminate = False
+        return terminate
+
+        '''
         else:
             current_goal_number = self.goal.goal_number
             last_goal = None
+            # 마지막 하위 목표 탐색
             for goal in GoalField().data:
                 if goal.get_higher_goal_number() == current_goal_number:
                     last_goal = goal
+            # 하위 목표가 존재하면 
             if last_goal != None:
                 goal_level = last_goal.get_goal_level()
                 last_part_number = last_goal.get_part_number(goal_level)
@@ -196,53 +228,63 @@ class AddGoalPage(EditGoalPage):
                     new_part_number = '01'
                 new_goal_number = current_goal_number + new_part_number
                 last_index = GoalField().data.index(last_goal)
+            # 하위 목표가 없는 경우, 1번 하위 목표 추가, 입력받은 내용 저장
             else:
                 new_goal_number = current_goal_number + '01'
                 last_index = GoalField().data.index(self.goal)
             GoalField().data.insert(last_index+1,Goal(new_goal_number))
             GoalField().contents[new_goal_number] = command
-        return False
-
+        
+        '''
+# 목표 삭제 페이지
 class DeleteGoalPage(EditGoalPage):
+    # 페이지 열기
     def open_page(self):
-        self.print_menu('목표 삭제')
+        self.print_page('목표 삭제')
         command = input()
-        #return_message = RunMenu(goal_data_field, todays_goal_list, command)
-        if(command == '0'):
+        
+        # 하위 목표가 존재하는지 확인
+        if(len(self.sub_goal_list) == 0):
+            print('목표가 존재하지 않습니다.')
+            self.delete_page()
+        # 0 입력시 메인 목표 페이지로 이동
+        elif(command == '0'):
             next_page = MainGoalPage()
             self.in_opened_pages(next_page)
         elif(command == '-1'):
             self.delete_page()
         elif(command == 'esc'):
-            return True
-        # 1 이상의 정수를 입력받을 경우 해당 목표 페이지 열기
-        # 같은 코드 반복, 함수화 고려
-        elif(len(self.sub_goal_list) == 0):
-            print('목표가 존재하지 않습니다.')
+            terminate = True
+            return terminate
+        
         else:
             try:
                 int_command = int(command)
             except:
                 pass
             else:
+                # 하위 목표 리스트의 번호 내의 정수 입력시
                 if (0 < int_command and int_command <= len(self.menu_list)):
-                    # 해당 목표와 모~ 든 하위 항목 : 해당 목표의 레벨까지 목표번호가 동일한 목표들을 삭제
+                    # 정말 삭제할지 확인받기
                     do_delete = input('정말 목표를 삭제하시겠습니까? (Y/N): ').lower()
                     if do_delete == 'y':
                         selected_goal = self.sub_goal_list[int_command - 1]
                         goal_level = selected_goal.get_goal_level()
+                        # 해당 목표와 모~ 든 하위 항목(해당 목표의 레벨까지 목표번호가 동일한 목표)들을 삭제
                         for goal in GoalField().data:
-                            #0 1 2 3
-                            #01234567
                             check_number = goal.goal_number[0:(goal_level + 1) * goal.DIGIT_SIZE]
                             if check_number == selected_goal.goal_number:
                                 GoalField().data.remove(goal)
                                 GoalField().contents.pop(goal.goal_number)
-                        return False
+                        terminate = False
+                        return terminate
                     elif do_delete == 'n':
-                        return False
+                        terminate = False
+                        return terminate
+            # 잘못 입력시
             print('잘못 입력하였습니다.')
-        return False
+        terminate = False
+        return terminate
         
 
 
